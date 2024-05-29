d06929c0eeebaec184cfc368a7bb9a7730584d99
Mateus Beltrão <mateusbeltrao2019@gmail.com>
2024-05-29 07:59:55 -0300

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,43 @@
 import React from 'react'
 import './Hero.css'
 import Emailbox from '../Emailbox/Emailbox'
+import { HeroData } from '@/src/utils/data'
 const Hero = () => {
   return(
     <div className='h-wrapper'>
         <div className='container'>
             <div className='h-container'>
-                <div className='h-left'>Left side</div>
+                <div className='h-left'>
+                    <div className='image-row'>
+                        {
+                            HeroData.slice(3, 6).map((person, i) => (
+                                <div className='person-pill' key={i}>
+                                    <div className='person-pill-bg'>
+                                        <img src={person.src} alt={person.src}></img>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                    <div className='image-row'>
+                        {
+                            HeroData.slice(0, 3).map((person, i) => (
+                                <div className='person-pill' key={i}>
+                                    <div className='person-pill-bg'>
+                                        <img src={person.src} alt={person.src}></img>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
 
                 <div className='h-right'>
                     <div className='h-title'>
-                        <span>Redefine How</span>
-                        <span>You Grow Your</span>
-                        <span>Digital Business</span>
+                        <span>REDEFINE HOW</span>
+                        <span>YOU GROW YOUR</span>
+                        <span>DIGITAL BUSINESS</span>
                     </div>
 
                     <div className='h-des'>
